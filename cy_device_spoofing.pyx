59f0e6d25c98c554fe14cff05ebc4fd9997d0ff2
nobisfxxx <suwannorman@gmail.com>
2025-04-14 10:13:17 +0530

    cy_device_spoofing.pyx

--- cy_device_spoofing.pyx
+++ cy_device_spoofing.pyx
@@ -1,12 +1,17 @@
-def get_device():
-    return {
-        "app_version": "272.0.0.18.84",
-        "android_version": 34,
-        "android_release": "14",
-        "dpi": "480dpi",
-        "resolution": "1080x2400",
-        "manufacturer": "infinix",
-        "device": "Infinix-X6739",
-        "model": "Infinix X6739",
-        "cpu": "mt6893"
-    }
+# cy_device_spoofing.pyx
+
+# Importing the random module using Cython's py_import
+from random import choice
+
+# Other code and function definitions
+
+# Example usage of random.choice
+def random_roast():
+    ROASTS = [
+        "You're like a cloud. When you disappear, it's a beautiful day!",
+        "If you were a vegetable, you'd be a 'cabbage'.",
+        "I would agree with you, but then we’d both be wrong.",
+        "Your secrets are always safe with me. I never even listen when you tell me them.",
+        "You bring everyone so much joy when you leave the room."
+    ]
+    return choice(ROASTS)  # Using random.choice here
