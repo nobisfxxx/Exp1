72de2fe27de5eb095fd12103ffec604f945f2f84
nobisfxxx <suwannorman@gmail.com>
2025-04-14 09:28:14 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
+extensions = [
+    Extension('bot', ['bot.py'])  # Adjust as needed
+]
+
 setup(
-    ext_modules=cythonize("cy_device_spoofing.pyx")
+    ext_modules=cythonize(extensions)
 )
