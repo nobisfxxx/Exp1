f224ce85d15babafc777bfc72fb87947ac6d2b2d
nobisfxxx <suwannorman@gmail.com>
2025-04-14 08:54:00 +0530

    cy_device_spoofing.pyx

---
+++ cy_device_spoofing.pyx
@@ -0,0 +1,33 @@
+# cy_device_spoofing.pyx
+
+# Importing necessary libraries
+import random
+import string
+
+# Function to generate random device fingerprint
+def generate_device_fingerprint():
+    """Generate a random fingerprint for device spoofing."""
+    manufacturers = ['Samsung', 'Apple', 'Google', 'Xiaomi', 'Infinix']
+    models = ['GT 10 Pro', 'Galaxy S20', 'Pixel 6', 'Redmi Note 11', 'Mi 11']
+
+    manufacturer = random.choice(manufacturers)
+    model = random.choice(models)
+    android_version = random.choice([13, 14, 15])  # Choose a random Android version
+    resolution = random.choice(['1080x2400', '1440x3200', '1080x2340'])
+    cpu = random.choice(['mt6893', 'snapdragon_888', 'mediatek_dimensity1200'])
+
+    # Generate random values for other fields
+    dpi = random.choice([320, 480])
+    device_id = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+
+    fingerprint = {
+        "manufacturer": manufacturer,
+        "model": model,
+        "android_version": android_version,
+        "resolution": resolution,
+        "cpu": cpu,
+        "dpi": dpi,
+        "device_id": device_id,
+    }
+
+    return fingerprint
