2f6bc596644f3b3a075b3d7e7bab8bf7e0a79784
nobisfxxx <suwannorman@gmail.com>
2025-04-24 10:39:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,5 +42,5 @@ def send_message(recipient_id, text):
     print("Sent:", response.text)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
