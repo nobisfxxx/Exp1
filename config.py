ad723fbc6189e5a6a02a79fe0d11e56a30548bfa
nobisfxxx <suwannorman@gmail.com>
2025-04-25 18:13:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+COOKIES_PATH = "cookies.json"
+
+DEVICE_INFO = {
+    "app_version": "272.0.0.18.84",
+    "android_version": 34,
+    "android_release": "14",
+    "dpi": "480dpi",
+    "resolution": "1080x2400",
+    "manufacturer": "infinix",
+    "device": "GT10Pro",
+    "model": "GT 10 Pro",
+    "cpu": "mediatek-dimensity8050"
+}
