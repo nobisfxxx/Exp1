512c62e2c3d88f4b23cb9149643f75c2cbbb8261
nobisfxxx <suwannorman@gmail.com>
2025-04-25 18:10:09 +0530

    Create roasts.py

---
+++ roasts.py
@@ -0,0 +1,9 @@
+ROAST_MESSAGES = [
+    "teri soch NASA se bhi zyada overpowered hai",
+    "tu chup reh... tujhe dekh ke filter bhi resign de de",
+    "Google bhi tera logic samajhne se fail ho gaya",
+    "tu rehne de... tera dimaag Airplane mode pe hai",
+    "teri harkatein dekh ke virus bhi confuse ho gaya",
+    "tu jab bolta hai... logic suicide kar leta hai",
+    "bhai tu toh Wikipedia ka opposite hai - 'Never reliable!'"
+]
